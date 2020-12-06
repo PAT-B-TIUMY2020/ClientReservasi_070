9903f2e56c116e9ddcb197443fc0214c6e9042c4
syafiraJafar <56532146+syafiraJafar@users.noreply.github.com>
2020-12-07 00:50:40 +0700

    Membuat CRUD pada Client

--- ClientReservasi-070/Form1.cs
+++ ClientReservasi-070/Form1.cs
@@ -12,9 +12,103 @@ namespace ClientReservasi_070
 {
     public partial class Form1 : Form
     {
+        ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
         public Form1()
         {
             InitializeComponent();
+            TampilData();
+            btUpdate.Enabled = false;
+            btHapus.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSimpan_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = textBoxID.Text;
+            string NamaCustomer = textBoxNama.Text;
+            string NoTelepon = textBoxNoTlf.Text;
+            int JumlahPemesanan = int.Parse(textBoxJumlah.Text);
+            string IdLokasi = textBoxIDLokasi.Text;
+
+            var a = service.pemesanan(IDPemesanan, NamaCustomer, NoTelepon, JumlahPemesanan, IdLokasi);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = textBoxID.Text;
+            string NamaCustomer = textBoxNama.Text;
+            string NoTelepon = textBoxNoTlf.Text;
+
+            var a = service.editPemesanan(IDPemesanan, NamaCustomer, NoTelepon);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        private void btHapus_Click(object sender, EventArgs e)
+        {
+            string IDPemesanan = textBoxID.Text;
+
+            var a = service.deletePemesanan(IDPemesanan);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        public void TampilData()
+        {
+            var List = service.Pemesanan1();
+            dtPemesanan.DataSource = List;
+        }
+
+        
+
+        public void Clear()
+        {
+            textBoxID.Clear();
+            textBoxNama.Clear();
+            textBoxNoTlf.Clear();
+            textBoxJumlah.Clear();
+            textBoxIDLokasi.Clear();
+
+            textBoxJumlah.Enabled = true;
+            textBoxIDLokasi.Enabled = true;
+
+            btSimpan.Enabled = true;
+            btUpdate.Enabled = false;
+            btHapus.Enabled = false;
+
+            textBoxID.Enabled = true;
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dtPemesanan_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxID.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[0].Value);
+            textBoxNama.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[3].Value);
+            textBoxNoTlf.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[4].Value);
+            textBoxJumlah.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[1].Value);
+            textBoxIDLokasi.Text = Convert.ToString(dtPemesanan.Rows[e.RowIndex].Cells[2].Value);
+
+            textBoxIDLokasi.Enabled = false;
+            textBoxJumlah.Enabled = false;
+
+            btUpdate.Enabled = true;
+            btHapus.Enabled = true;
+
+            btSimpan.Enabled = false;
+            textBoxID.Enabled = false;
         }
     }
 }
